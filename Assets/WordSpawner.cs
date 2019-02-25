5b928418244acf2bcb43eb0053107204a38c2ba9
Rick <3741326+RickJP@users.noreply.github.com>
2019-02-25 17:14:55 +0900

    initial commit

---
+++ Assets/WordSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour
+{
+
+    public GameObject wordPrefab;
+
+    public WordDisplay SpawnWord()
+    {
+        GameObject wordObj = Instantiate(wordPrefab);
+        WordDisplay wordDisplay = wordObj.GetComponent<WordDisplay>();
+
+        return wordDisplay;
+    }
+}
